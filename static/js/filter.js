6c50ca79cbc2c71b2d3c16fe58f59626f85232d6
Tas <tas0173@gmail.com>
2020-10-26 17:01:41 +1100

    Update product filter

--- static/js/filter.js
+++ static/js/filter.js
@@ -18,41 +18,43 @@ function myStoreFunction() {
   }
 }
 
-function myCategoryFunction() {
-    var input, filter, table, tr, td, x;
-    input = document.getElementById("UserInput");
-    filter = input.value.toLowerCase();
-    table = document.getElementById("CategoryTable");
-    tr = table.getElementsByTagName("tr");
+// function myCategoryFunction() {
+//     var input, filter, table, tr, td, x;
+//     input = document.getElementById("UserInput");
+//     filter = input.value.toLowerCase();
+//     table = document.getElementById("CategoryTable");
+//     tr = table.getElementsByTagName("tr");
   
-    for (x = 0; x < tr.length; x++) {
-      td = tr[x].getElementsByTagName("td")[0];
-      if (td) {
-        if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
-          tr[x].style.display = "";
-        }
-        else {
-          tr[x].style.display = "none";
-        }
-      }
-    }
-  }
+//     for (x = 0; x < tr.length; x++) {
+//       td = tr[x].getElementsByTagName("td")[0];
+//       if (td) {
+//         if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
+//           tr[x].style.display = "";
+//         }
+//         else {
+//           tr[x].style.display = "none";
+//         }
+//       }
+//     }
+//   }
+
+
 
 function myProductFunction() {
-    var input, filter, table, tr, td, x;
-    input = document.getElementById("UserInput");
+    var input, filter, table, selProd, filProd, x;
+    input = document.getElementById("inputProduct");
     filter = input.value.toLowerCase();
-    table = document.getElementById("ProductTable");
-    tr = table.getElementsByTagName("tr");
+    table = document.getElementById("productName");
+    selProd = table.getElementsByTagName("option");
   
-    for (x = 0; x < tr.length; x++) {
-      td = tr[x].getElementsByTagName("td")[0];
-      if (td) {
-        if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
-          tr[x].style.display = "";
+    for (x = 0; x < selProd.length; x++) {
+      filProd = selProd[x].getElementsByTagName("option")[0];
+      if (filProd) {
+        if (filProd.innerHTML.toLowerCase().indexOf(filter) > -1) {
+          selProd[x].style.display = "";
         }
         else {
-          tr[x].style.display = "none";
+          selProd[x].style.display = "none";
         }
       }
     }
