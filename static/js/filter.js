fc0d98b8d57d7abd6d3840e9d382432a99642b2c
Jamie-GiHu <65794716+Jamie-GiHu@users.noreply.github.com>
2020-10-27 20:03:21 +1100

    Update filter.js

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,22 +1,22 @@
-function myStoreFunction() {
-  var input, filter, table, tr, td, x;
-  input = document.getElementById("UserInput");
-  filter = input.value.toLowerCase();
-  table = document.getElementById("CustomerTable"); 
-  tr = table.getElementsByTagName("tr");
+// function myStoreFunction() {
+//   var input, filter, table, tr, td, x;
+//   input = document.getElementById("UserInput");
+//   filter = input.value.toLowerCase();
+//   table = document.getElementById("CustomerTable"); 
+//   tr = table.getElementsByTagName("tr");
 
-  for (x = 0; x < tr.length; x++) {
-    td = tr[x].getElementsByTagName("td")[0];
-    if (td) {
-      if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
-        tr[x].style.display = "";
-      }
-      else {
-        tr[x].style.display = "none";
-      }
-    }
-  }
-}
+//   for (x = 0; x < tr.length; x++) {
+//     td = tr[x].getElementsByTagName("td")[0];
+//     if (td) {
+//       if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
+//         tr[x].style.display = "";
+//       }
+//       else {
+//         tr[x].style.display = "none";
+//       }
+//     }
+//   }
+// }
 
 // function myCategoryFunction() {
 //     var input, filter, table, tr, td, x;
