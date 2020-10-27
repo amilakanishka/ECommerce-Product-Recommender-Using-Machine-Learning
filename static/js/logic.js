ee1f6c1e159e280a8d45789120acb9908690313b
Tas <tas0173@gmail.com>
2020-10-27 17:01:24 +1100

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,79 +1,79 @@
-d3.select("#submit").on("click", handleSubmit);
+// d3.select("#submit").on("click", handleSubmit);
 
-function handleSubmit() {
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
+// function handleSubmit() {
+//   // Prevent the page from refreshing
+//   d3.event.preventDefault();
 
-  // Select the input value from the form
-  var customerSelection = d3.select("#customer_name").node().value;
-  var productSelection = d3.select("#product_category").node().value;
-  var productName = d3.select("#product_name").node().value;
+//   // Select the input value from the form
+//   var customerSelection = d3.select("#customer_name").node().value;
+//   var productSelection = d3.select("#product_category").node().value;
+//   var productName = d3.select("#product_name").node().value;
 
-  var url = `/get_recommendations/${customerSelection}/${productSelection}/${productName}`;
-  d3.json(url).then(function(data) {
-    renderProdRecTable(data,customerSelection,productSelection,productName);
-    myProductFunction(data, productName)
-  });
-}
+//   var url = `/get_recommendations/${customerSelection}/${productSelection}/${productName}`;
+//   d3.json(url).then(function(data) {
+//     renderProdRecTable(data,customerSelection,productSelection,productName);
+//     myProductFunction(data, productName)
+//   });
+// }
 
-// Rendering the machine learned products XXXXXXXXX fix line 23 and 24
-function renderProdRecTable(prodListing){
-  d3.select("#recommend-product").selectAll("div").remove();
-  var url = `/get_recommendations/${customerSelection}/${productSelection}/${productName}`;
-  d3.json(url).then(function(productList) {
-    prodListing = prodListing.filter(prod => prod.XXXXX);
-    var loopMax = Math.min(prodListing.length, 5);
-        for (i = 0; i < loopMax; i++) {
+// // Rendering the machine learned products XXXXXXXXX fix line 23 and 24
+// function renderProdRecTable(prodListing){
+//   d3.select("#recommend-product").selectAll("div").remove();
+//   var url = `/get_recommendations/${customerSelection}/${productSelection}/${productName}`;
+//   d3.json(url).then(function(productList) {
+//     prodListing = prodListing.filter(prod => prod.XXXXX);
+//     var loopMax = Math.min(prodListing.length, 5);
+//         for (i = 0; i < loopMax; i++) {
     
-    // Get data and insert into variable
-    var itemDescription = prodListing[i].item_description;
-    var itemCost = prodListing[i].item_cost;
-    var itemImage = prodListing[i].item_url;
+//     // Get data and insert into variable
+//     var itemDescription = prodListing[i].item_description;
+//     var itemCost = prodListing[i].item_cost;
+//     var itemImage = prodListing[i].item_url;
 
-    // Create a div container to hold the product details
-    var containerDiv = document.createElement('div');
-    containerDiv.id = 'prod-container';
-    containerDiv.className = 'container';
+//     // Create a div container to hold the product details
+//     var containerDiv = document.createElement('div');
+//     containerDiv.id = 'prod-container';
+//     containerDiv.className = 'container';
 
-    // Create row div
-    var rowDiv = document.createElement('div');
-    rowDiv.className = 'row';
+//     // Create row div
+//     var rowDiv = document.createElement('div');
+//     rowDiv.className = 'row';
 
-    // DIV FOR PRODUCT LISTING
-    // Create div for bootstrap
-    var bootstrapDivLeft = document.createElement('div');
-    bootstrapDivLeft.id = 'insertRecommendationHere';
-    bootstrapDivLeft.className = 'col-md-6 mx-auto';
+//     // DIV FOR PRODUCT LISTING
+//     // Create div for bootstrap
+//     var bootstrapDivLeft = document.createElement('div');
+//     bootstrapDivLeft.id = 'insertRecommendationHere';
+//     bootstrapDivLeft.className = 'col-md-6 mx-auto';
 
-    // Create h3 for product description
-    var titleH3 =  document.createElement('h3');
-    titleH3.innerText = itemDescription;
+//     // Create h3 for product description
+//     var titleH3 =  document.createElement('h3');
+//     titleH3.innerText = itemDescription;
 
-    // Create h3 for product cost
-    var costH3 =  document.createElement('h3');
-    costH3.innerText = itemCost;
+//     // Create h3 for product cost
+//     var costH3 =  document.createElement('h3');
+//     costH3.innerText = itemCost;
 
-    // DIV FOR IMAGE
-    // Create div for bootstrap 
-    var bootstrapDivRight = document.createElement('div');
-    bootstrapDivRight.id = 'insertImageHere'+i;
-    bootstrapDivRight.className = 'col-md-6 product-image';
+//     // DIV FOR IMAGE
+//     // Create div for bootstrap 
+//     var bootstrapDivRight = document.createElement('div');
+//     bootstrapDivRight.id = 'insertImageHere'+i;
+//     bootstrapDivRight.className = 'col-md-6 product-image';
 
-    // Create h3 for product description
-    var titleH3 =  document.createElement('src');
-    titleH3.innerText = itemImage;
+//     // Create h3 for product description
+//     var titleH3 =  document.createElement('src');
+//     titleH3.innerText = itemImage;
 
-    // Append child 
-    containerDiv.appendChild(rowDiv);
-    rowDiv.appendChild(bootstrapDivLeft);
-    rowDiv.appendChild(bootstrapDivRight);
+//     // Append child 
+//     containerDiv.appendChild(rowDiv);
+//     rowDiv.appendChild(bootstrapDivLeft);
+//     rowDiv.appendChild(bootstrapDivRight);
 
-    // Then append the whole thing onto the section
-    document.getElementById('recommend-product').appendChild(containerDiv);
+//     // Then append the whole thing onto the section
+//     document.getElementById('recommend-product').appendChild(containerDiv);
 
     
 
-  };
+//   };
 
 // Dynamic product search
 // function myProductFunction() {
