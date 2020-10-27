6914e08d211960a409ab5b49ece92894e1944c8f
Jamie-GiHu <65794716+Jamie-GiHu@users.noreply.github.com>
2020-10-27 21:48:41 +1100

    removed unwanted files

--- static/js/filter.js
+++ static/js/filter.js
@@ -39,6 +39,29 @@
 //   }
 
 
+selProd.onkeyup = function() {myProductFunction()};
+
+function myProductFunction() {
+  
+  // Declare variables
+  var input, filter, div, option, i, txtValue;
+  input = document.getElementById('product_name');
+  filter = input.value.toLowerCase();
+  ul = document.getElementById("productUL");
+  option = div.getElementsByTagName('option');
+  
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < option.length; i++) {
+    txtValue = option[i].textContent || option[i].innerText;
+    if (txtValue.toLowerCase().indexOf(filter) > -1) {
+      option[i].style.display = "";
+    } else {
+      option[i].style.display = "none";
+    }
+  }
+  
+}
+
 
 
 
