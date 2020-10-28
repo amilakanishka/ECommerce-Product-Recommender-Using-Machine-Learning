c10b280a2db28e9c03aafd0a8b95b7b3a9f7e672
Tas <tas0173@gmail.com>
2020-10-28 14:44:24 +1100

    Update Filters

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,42 +1,50 @@
-// function myStoreFunction() {
-//   var input, filter, table, tr, td, x;
-//   input = document.getElementById("UserInput");
-//   filter = input.value.toLowerCase();
-//   table = document.getElementById("CustomerTable"); 
-//   tr = table.getElementsByTagName("tr");
+// Create event listener to tigger myStoreFunction()
+var selStore = document.getElementById('store_number');
+selStore.onkeyup = function() {myStoreFunction()};
 
-//   for (x = 0; x < tr.length; x++) {
-//     td = tr[x].getElementsByTagName("td")[0];
-//     if (td) {
-//       if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
-//         tr[x].style.display = "";
-//       }
-//       else {
-//         tr[x].style.display = "none";
-//       }
-//     }
-//   }
-// }
-
-// function myCategoryFunction() {
-//     var input, filter, table, tr, td, x;
-//     input = document.getElementById("UserInput");
-//     filter = input.value.toLowerCase();
-//     table = document.getElementById("CategoryTable");
-//     tr = table.getElementsByTagName("tr");
+// Define function to filter product name list on user input
+function myStoreFunction() {
   
-//     for (x = 0; x < tr.length; x++) {
-//       td = tr[x].getElementsByTagName("td")[0];
-//       if (td) {
-//         if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
-//           tr[x].style.display = "";
-//         }
-//         else {
-//           tr[x].style.display = "none";
-//         }
-//       }
-//     }
-//   }
+  // Declare variables
+  var input = document.getElementById('store_number');
+  var filter = input.value.toLowerCase();
+  var ul = document.getElementById("storeUL");
+  var option = ul.getElementsByTagName('option');
+  
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < option.length; i++) {
+    var txtValue = option[i].textContent || option[i].innerText;
+    if (txtValue.toLowerCase().indexOf(filter) > -1) {
+      option[i].style.display = "";
+    } else {
+      option[i].style.display = "none";
+    }
+  }
+}
+
+// Create event listener to tigger myCategoryFunction()
+var selCat = document.getElementById('product_category');
+selCat.onkeyup = function() {myCategoryFunction()};
+
+// Define function to filter product name list on user input
+function myCategoryFunction() {
+  
+  // Declare variables
+  var input = document.getElementById('product_category');
+  var filter = input.value.toLowerCase();
+  var ul = document.getElementById("catUL");
+  var option = ul.getElementsByTagName('option');
+  
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < option.length; i++) {
+    var txtValue = option[i].textContent || option[i].innerText;
+    if (txtValue.toLowerCase().indexOf(filter) > -1) {
+      option[i].style.display = "";
+    } else {
+      option[i].style.display = "none";
+    }
+  }
+}
 
 // Create event listener to tigger myProductFunction()
 var selProd = document.getElementById('product_name');
