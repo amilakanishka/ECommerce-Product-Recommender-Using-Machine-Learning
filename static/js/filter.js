53285cccfb8bffa5febbcbb342c0eda4e9218efa
Jamie-GiHu <65794716+Jamie-GiHu@users.noreply.github.com>
2020-10-29 11:37:09 +1100

    Update filter.js
    block lines for myStoreFunction()

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,26 +1,26 @@
-// Create event listener to tigger myStoreFunction()
-var selStore = document.getElementById('store_number');
-selStore.onkeyup = function() {myStoreFunction()};
+// // Create event listener to tigger myStoreFunction()
+// var selStore = document.getElementById('store_number');
+// selStore.onkeyup = function() {myStoreFunction()};
 
-// Define function to filter product name list on user input
-function myStoreFunction() {
+// // Define function to filter product name list on user input
+// function myStoreFunction() {
   
-  // Declare variables
-  var input = document.getElementById('store_number');
-  var filter = input.value.toLowerCase();
-  var ul = document.getElementById("storeUL");
-  var option = ul.getElementsByTagName('option');
+//   // Declare variables
+//   var input = document.getElementById('store_number');
+//   var filter = input.value.toLowerCase();
+//   var ul = document.getElementById("storeUL");
+//   var option = ul.getElementsByTagName('option');
   
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < option.length; i++) {
-    var txtValue = option[i].textContent || option[i].innerText;
-    if (txtValue.toLowerCase().indexOf(filter) > -1) {
-      option[i].style.display = "";
-    } else {
-      option[i].style.display = "none";
-    }
-  }
-}
+//   // Loop through all list items, and hide those who don't match the search query
+//   for (i = 0; i < option.length; i++) {
+//     var txtValue = option[i].textContent || option[i].innerText;
+//     if (txtValue.toLowerCase().indexOf(filter) > -1) {
+//       option[i].style.display = "";
+//     } else {
+//       option[i].style.display = "none";
+//     }
+//   }
+// }
 
 // Create event listener to tigger myCategoryFunction()
 var selCat = document.getElementById('product_category');
