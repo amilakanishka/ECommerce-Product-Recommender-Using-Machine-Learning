05aa498ca8073df16316f40ccb3b98985bf9c834
Jamie-GiHu <65794716+Jamie-GiHu@users.noreply.github.com>
2020-10-29 12:47:36 +1100

    Update filter.js
    comment out all filtering functions

--- static/js/filter.js
+++ static/js/filter.js
@@ -22,54 +22,54 @@
 //   }
 // }
 
-// Create event listener to tigger myCategoryFunction()
-var selCat = document.getElementById('product_category');
-selCat.onkeyup = function() {myCategoryFunction()};
+// // Create event listener to tigger myCategoryFunction()
+// var selCat = document.getElementById('product_category');
+// selCat.onkeyup = function() {myCategoryFunction()};
 
-// Define function to filter product name list on user input
-function myCategoryFunction() {
+// // Define function to filter product name list on user input
+// function myCategoryFunction() {
   
-  // Declare variables
-  var input = document.getElementById('product_category');
-  var filter = input.value.toLowerCase();
-  var ul = document.getElementById("catUL");
-  var option = ul.getElementsByTagName('option');
+//   // Declare variables
+//   var input = document.getElementById('product_category');
+//   var filter = input.value.toLowerCase();
+//   var ul = document.getElementById("catUL");
+//   var option = ul.getElementsByTagName('option');
   
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < option.length; i++) {
-    var txtValue = option[i].textContent || option[i].innerText;
-    if (txtValue.toLowerCase().indexOf(filter) > -1) {
-      option[i].style.display = "";
-    } else {
-      option[i].style.display = "none";
-    }
-  }
-}
+//   // Loop through all list items, and hide those who don't match the search query
+//   for (i = 0; i < option.length; i++) {
+//     var txtValue = option[i].textContent || option[i].innerText;
+//     if (txtValue.toLowerCase().indexOf(filter) > -1) {
+//       option[i].style.display = "";
+//     } else {
+//       option[i].style.display = "none";
+//     }
+//   }
+// }
 
-// Create event listener to tigger myProductFunction()
-var selProd = document.getElementById('product_name');
-selProd.onkeyup = function() {myProductFunction()};
+// // Create event listener to tigger myProductFunction()
+// var selProd = document.getElementById('product_name');
+// selProd.onkeyup = function() {myProductFunction()};
 
-// Define function to filter product name list on user input
-function myProductFunction() {
+// // Define function to filter product name list on user input
+// function myProductFunction() {
   
-  // Declare variables
-  var input = document.getElementById('product_name');
-  var filter = input.value.toLowerCase();
-  var ul = document.getElementById("productUL");
-  var option = ul.getElementsByTagName('option');
+//   // Declare variables
+//   var input = document.getElementById('product_name');
+//   var filter = input.value.toLowerCase();
+//   var ul = document.getElementById("productUL");
+//   var option = ul.getElementsByTagName('option');
   
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < option.length; i++) {
-    var txtValue = option[i].textContent || option[i].innerText;
-    if (txtValue.toLowerCase().indexOf(filter) > -1) {
-      option[i].style.display = "";
-    } else {
-      option[i].style.display = "none";
-    }
-  }
+//   // Loop through all list items, and hide those who don't match the search query
+//   for (i = 0; i < option.length; i++) {
+//     var txtValue = option[i].textContent || option[i].innerText;
+//     if (txtValue.toLowerCase().indexOf(filter) > -1) {
+//       option[i].style.display = "";
+//     } else {
+//       option[i].style.display = "none";
+//     }
+//   }
   
-}
+// }
 
 // RECOMMENDER FUNCTION HERE
 
