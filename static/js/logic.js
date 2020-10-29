ebdf605c27baabebb0ec072cea2d4ee3d297495a
Tas <tas0173@gmail.com>
2020-10-29 15:15:06 +1100

    Remove duplicate recommender function

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,49 +8,49 @@ function handleSubmit() {
 
 // RECOMMENDER FUNCTION HERE
 // Iowa file path
-var iowaPath = "../static/data/iowa.csv" 
-d3.csv(iowaPath)
-  .then(data => {var iowaList = data;
-  console.log("iowaList");
-  console.log(iowaList);
+// var iowaPath = "../static/data/iowa.csv" 
+// d3.csv(iowaPath)
+//   .then(data => {var iowaList = data;
+//   console.log("iowaList");
+//   console.log(iowaList);
 
-  // TEMP ONLY - replace with recommender data later
-  // Place 8 products for now *** can change
-  for (i = 0; i < 8; i++) {
-    // Get data and insert into variable
-    var imageUrl = iowaList[i].image_url;
-    var productName = iowaList[i].item_description;
-    // Create container div for product detail
-    var containerDiv = document.createElement('div');
-    containerDiv.className = 'container';
+//   // TEMP ONLY - replace with recommender data later
+//   // Place 8 products for now *** can change
+//   for (i = 0; i < 8; i++) {
+//     // Get data and insert into variable
+//     var imageUrl = iowaList[i].image_url;
+//     var productName = iowaList[i].item_description;
+//     // Create container div for product detail
+//     var containerDiv = document.createElement('div');
+//     containerDiv.className = 'container';
     
-    // // Create row div
-    // var rowDiv = document.createElement('div');
-    // rowDiv.className = 'row';
+//     // // Create row div
+//     // var rowDiv = document.createElement('div');
+//     // rowDiv.className = 'row';
     
-    // DIV FOR PRODUCT LISTING
-    // Create div for bootstrap
-    var bootstrapDivLeft = document.createElement('div');
-    bootstrapDivLeft.id = 'insertProductHere';
-    bootstrapDivLeft.className = 'col-md-4';
-    // Create img for product image
-    var productImg = document.createElement('img');
-    productImg.src = imageUrl;
-    // Create h3 for product name
-    var titleH3 =  document.createElement('h3');
-    titleH3.innerText = productName;
+//     // DIV FOR PRODUCT LISTING
+//     // Create div for bootstrap
+//     var bootstrapDivLeft = document.createElement('div');
+//     bootstrapDivLeft.id = 'insertProductHere';
+//     bootstrapDivLeft.className = 'col-md-3 mx-auto';
+//     // Create img for product image
+//     var productImg = document.createElement('img');
+//     productImg.src = imageUrl;
+//     // Create h3 for product name
+//     var titleH3 =  document.createElement('h3');
+//     titleH3.innerText = productName;
     
-    // Append child
-    // containerDiv.appendChild(rowDiv);
-    // rowDiv.appendChild(bootstrapDivLeft);
-    containerDiv.appendChild(bootstrapDivLeft);
-    bootstrapDivLeft.appendChild(productImg);
-    bootstrapDivLeft.appendChild(titleH3);
+//     // Append child
+//     // containerDiv.appendChild(rowDiv);
+//     // rowDiv.appendChild(bootstrapDivLeft);
+//     containerDiv.appendChild(bootstrapDivLeft);
+//     bootstrapDivLeft.appendChild(productImg);
+//     bootstrapDivLeft.appendChild(titleH3);
     
-    // Then append the whole thing onto the recommend-product section
-    document.getElementById('recommend-product').appendChild(containerDiv);
-  }
-  })
+//     // Then append the whole thing onto the recommend-product section
+//     document.getElementById('recommend-product').appendChild(containerDiv);
+//   }
+//   })
 
 // document.querySelector(document).ready(function(){
 //   document.querySelector("#product_name").addEventListener("keyup", function() {
