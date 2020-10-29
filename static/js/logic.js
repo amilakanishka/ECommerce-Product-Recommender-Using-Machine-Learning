d7e4967d075417d4e3056c5bd18b5e07f3278162
Tas <tas0173@gmail.com>
2020-10-29 22:04:30 +1100

    Test it works

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,63 +1,96 @@
 d3.select("#submit").on("click", handleSubmit);
 
 function handleSubmit() {
-  // Prevent the page from refreshing
+//   // Prevent the page from refreshing
   d3.event.preventDefault();
+
+// Select the input value from the index.html
+  var productSiteSelector = d3.select("#productUl").node().value; 
+  var iowaPath = `/get_recommendations_for_product_selection/${productSiteSelector}`
+  console.log(productSiteSelector);
+  var alcoholIowa = d3.select("#productUl").property("value");
+  console.log(alcoholIowa);
 
 
 
 // RECOMMENDER FUNCTION HERE
-// Iowa file path
+
+// Set global variable
+var iowaList;
+
+// If read from csv
 // var iowaPath = "../static/data/iowa.csv" 
 // d3.csv(iowaPath)
 //   .then(data => {var iowaList = data;
-//   console.log("iowaList");
-//   console.log(iowaList);
 
-//   // TEMP ONLY - replace with recommender data later
-//   // Place 8 products for now *** can change
-//   for (i = 0; i < 8; i++) {
-//     // Get data and insert into variable
-//     var imageUrl = iowaList[i].image_url;
-//     var productName = iowaList[i].item_description;
-//     // Create container div for product detail
-//     var containerDiv = document.createElement('div');
-//     containerDiv.className = 'container';
-    
-//     // // Create row div
-//     // var rowDiv = document.createElement('div');
-//     // rowDiv.className = 'row';
-    
-//     // DIV FOR PRODUCT LISTING
-//     // Create div for bootstrap
-//     var bootstrapDivLeft = document.createElement('div');
-//     bootstrapDivLeft.id = 'insertProductHere';
-//     bootstrapDivLeft.className = 'col-md-3 mx-auto';
-//     // Create img for product image
-//     var productImg = document.createElement('img');
-//     productImg.src = imageUrl;
-//     // Create h3 for product name
-//     var titleH3 =  document.createElement('h3');
-//     titleH3.innerText = productName;
-    
-//     // Append child
-//     // containerDiv.appendChild(rowDiv);
-//     // rowDiv.appendChild(bootstrapDivLeft);
-//     containerDiv.appendChild(bootstrapDivLeft);
-//     bootstrapDivLeft.appendChild(productImg);
-//     bootstrapDivLeft.appendChild(titleH3);
-    
-//     // Then append the whole thing onto the recommend-product section
-//     document.getElementById('recommend-product').appendChild(containerDiv);
-//   }
-//   })
+// If read from Heroku json
+// Iowa file path
+// var iowaPath = `/get_recommendations_for_product_selection/${productSelection}`;
+// var iowaPath = `/get_recommendations_for_product_selection/966136`;
 
-// document.querySelector(document).ready(function(){
-//   document.querySelector("#product_name").addEventListener("keyup", function() {
-//     var value = document.querySelector(this).value.toLowerCase();
-//     document.querySelector("#prodDiv *").filter(function() {
-//       document.querySelector(this).toggle(document.querySelector(this).text().toLowerCase().indexOf(value) > -1)
-//     });
-//   });
-// })
+d3.json(iowaPath)
+  .then(data => {iowaList = data; 
+
+  console.log("iowaList");
+  console.log(iowaList);
+  
+  // TEMP ONLY - replace with recommender data later
+  // Place 8 products for now *** can change
+  for (i = 0; i < 8; i++) {
+
+    // Get data and insert into variable
+    var imageUrl = iowaList[i].image_url; // for image url
+    var productName = iowaList[i].item_description; // for product name
+    var productNo = iowaList[i].item_number; // for product number
+    var categoryName = iowaList[i].category_name; // for category name
+    var categoryNo = iowaList[i].category; // for category number
+    var price = iowaList[i].price; // for price
+    var volume = iowaList[i].volume; // for volume
+
+    // Create container div for product detail
+    var containerDiv = document.createElement('div');
+    containerDiv.className = 'containerProd';
+
+    // DIV FOR PRODUCT LISTING
+    // Create div for bootstrap
+    var bootstrapDivLeft = document.createElement('div');
+    bootstrapDivLeft.id = 'insertProductHere';
+    // bootstrapDivLeft.className = 'col-md-3';
+
+    // Create img for product image
+    var productImg = document.createElement('img');
+    productImg.src = imageUrl;
+
+    // Create h5 for product name
+    var titleH5 =  document.createElement('h5');
+    titleH5.id = productNo;
+    titleH5.innerText = productName;
+
+    // Create ul for product category
+    var categoryUl =  document.createElement('p');
+    categoryUl.id = categoryNo;
+    categoryUl.innerText = categoryName;
+
+    // Create ul for price
+    var priceUl =  document.createElement('p');
+    priceUl.innerText = `$${price.toFixed(2)}`;
+  
+    // Create ul for volume
+    var volumeUl =  document.createElement('p');
+    volumeUl.innerText = `${volume} ml`;
+
+    // Append child
+    containerDiv.appendChild(bootstrapDivLeft);
+    bootstrapDivLeft.appendChild(productImg);
+    bootstrapDivLeft.appendChild(titleH5);
+    bootstrapDivLeft.appendChild(categoryUl);
+    bootstrapDivLeft.appendChild(priceUl);
+    bootstrapDivLeft.appendChild(volumeUl);
+
+    // Then append the whole thing onto the recommend-product section
+    document.getElementById('recommend-product').appendChild(containerDiv);
+
+  }
+  
+  })
 }
