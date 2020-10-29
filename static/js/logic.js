5ca54f8d2975c4a9653114121240951bb42446f5
Tas <tas0173@gmail.com>
2020-10-29 14:56:17 +1100

    bootstrap change

--- static/js/logic.js
+++ static/js/logic.js
@@ -32,7 +32,7 @@ d3.csv(iowaPath)
     // Create div for bootstrap
     var bootstrapDivLeft = document.createElement('div');
     bootstrapDivLeft.id = 'insertProductHere';
-    bootstrapDivLeft.className = 'col-md-3 mx-auto';
+    bootstrapDivLeft.className = 'col-md-3';
     // Create img for product image
     var productImg = document.createElement('img');
     productImg.src = imageUrl;
