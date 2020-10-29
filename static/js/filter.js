90527e20c89a98b069f947b59a03d712c58c1ad7
Jamie-GiHu <65794716+Jamie-GiHu@users.noreply.github.com>
2020-10-29 11:15:04 +1100

    Update filter.js
    try recommender function

--- static/js/filter.js
+++ static/js/filter.js
@@ -71,3 +71,67 @@ function myProductFunction() {
   
 }
 
+// RECOMMENDER FUNCTION HERE
+
+// Iowa file path
+
+// If read from csv
+// var iowaPath = "../static/data/iowa.csv" 
+// d3.csv(iowaPath)
+//   .then(data => {var iowaList = data;
+
+// If read from Heroku json
+// var iowaPath = `/get_recommendations_for_product_selection/${productSelection}`;
+var iowaPath = `/get_recommendations_for_product_selection/966136`;
+
+d3.json(url)
+  .then(data => {var iowaList = data; 
+
+  
+  console.log("iowaList");
+  console.log(iowaList);
+  
+  // TEMP ONLY - replace with recommender data later
+  // Place 8 products for now *** can change
+  for (i = 0; i < 8; i++) {
+
+    // Get data and insert into variable
+    var imageUrl = iowaList[i].image_url;
+    var productName = iowaList[i].item_description;
+
+    // Create container div for product detail
+    var containerDiv = document.createElement('div');
+    containerDiv.className = 'container';
+
+    // // Create row div
+    // var rowDiv = document.createElement('div');
+    // rowDiv.className = 'row';
+
+    // DIV FOR PRODUCT LISTING
+    // Create div for bootstrap
+    var bootstrapDivLeft = document.createElement('div');
+    bootstrapDivLeft.id = 'insertProductHere';
+    bootstrapDivLeft.className = 'col-md-3 mx-auto';
+
+    // Create img for product image
+    var productImg = document.createElement('img');
+    productImg.src = imageUrl;
+
+    // Create h3 for product name
+    var titleH3 =  document.createElement('h3');
+    titleH3.innerText = productName;
+
+    // Append child
+    // containerDiv.appendChild(rowDiv);
+    // rowDiv.appendChild(bootstrapDivLeft);
+    containerDiv.appendChild(bootstrapDivLeft);
+    bootstrapDivLeft.appendChild(productImg);
+    bootstrapDivLeft.appendChild(titleH3);
+
+    // Then append the whole thing onto the recommend-product section
+    document.getElementById('recommend-product').appendChild(containerDiv);
+
+  }
+  
+  })
+
