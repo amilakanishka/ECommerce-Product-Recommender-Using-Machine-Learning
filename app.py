1fb0d0c64af1d4160e179af904b4b5e12114ac88
amilakanishka <amilakanishka@gmail.com>
2020-10-30 21:03:21 +1100

    added popular model

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask import Flask, request, jsonify, render_template, redirect
 import turicreate as tc
 
 modelC = tc.load_model("drinkupiowa-C")
+modelPopular = tc.load_model("drinkupiowa-C-popular")
 
 app = Flask(__name__)
 
@@ -149,6 +150,17 @@ def get_recommendations_for_cart():
     
     data = get_product_details(prod_list)
     return jsonify(list(data))     
+
+@app.route("/get_popularity_recommendations", methods=["GET"])
+def get_popularity_recommendations():
+
+    data1 = modelPopular.recommend()
+    prod_list = []
+    for prod in list(data1):
+        prod_list.append(prod['StockCode'])
+    
+    data = get_product_details(prod_list)
+    return jsonify(data)     
 
 if __name__ == '__main__':
     app.run()    
